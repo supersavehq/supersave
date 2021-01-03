38ef3e00a0f0f8c7de649042de168611293040d8
daangemist <git@daangemist.nl>
2021-01-03 23:11:11 +0100

    Implemented sort parameter for http querying.

--- src/collection/Http/actions/get.ts
+++ src/collection/Http/actions/get.ts
@@ -2,10 +2,26 @@ import { Response, Request } from 'express';
 import { Query } from '../../../database/EntityManager';
 import { ManagedCollection } from '../../types';
 
+function sort(query: Query, sortRequest: string): void {
+  const sorts = sortRequest.split(',');
+  sorts.forEach((sortField: string) => {
+    if (sortField.startsWith('-')) {
+      query.sort(sortField.substring(1));
+    } else {
+      query.sort(sortField);
+    }
+  });
+}
+
 export default (collection: ManagedCollection): (req: Request, res: Response) => Promise<void> =>
   // eslint-disable-next-line implicit-arrow-linebreak
-  async (_req: Request, res: Response): Promise<void> => {
+  async (req: Request, res: Response): Promise<void> => {
     const query: Query = collection.repository.createQuery();
+
+    if (req.query.sort) {
+      sort(query, (req.query.sort as string));
+    }
+
     const items = await collection.repository.getByQuery(query.limit(25));
     res.json({ data: items });
   };
--- tests/integration/collection/http/get.test.ts
+++ tests/integration/collection/http/get.test.ts
@@ -2,8 +2,8 @@
 import supertest from 'supertest';
 import express from 'express';
 import { Planet, Moon } from '../../../types';
-import { planetCollection, moonCollection } from '../../../entities';
-import { SuperSave } from '../../../../build';
+import { planetCollection, moonCollection, planetEntity } from '../../../entities';
+import { Repository, SuperSave } from '../../../../build';
 
 test('empty collection returns empty array', async() => {
   const app: express.Application = express();
@@ -21,4 +21,68 @@ test('empty collection returns empty array', async() => {
   expect(response.body.data).toBeDefined();
   expect(Array.isArray(response.body.data)).toBe(true);
   expect(response.body.data).toHaveLength(0);
+});
+
+test('collection items are returned', async() => {
+  const app: express.Application = express();
+  const superSave = await SuperSave.create(':memory:');
+
+  const repository: Repository<Planet> = await superSave.addCollection<Planet>(planetCollection);
+  app.use('/', superSave.getRouter());
+
+  await repository.create({ name: 'Earth' });
+  const response = await supertest(app)
+    .get('/planets')
+    .expect('Content-Type', /json/)
+    .expect(200);
+
+  expect(response.body.data).toBeDefined();
+  expect(Array.isArray(response.body.data)).toBe(true);
+  expect(response.body.data).toHaveLength(1);
+  expect(response.body.data[0].name).toBe('Earth');
+});
+
+describe('collection items are sorted when requested', async() => {
+  const app: express.Application = express();
+  const superSave = await SuperSave.create(':memory:');
+
+  const repository: Repository<Planet> = await superSave.addCollection<Planet>({
+    ...planetCollection,
+    entity: {
+      ...planetEntity,
+      filterSortFields: { name: 'string' }
+    }
+  });
+  app.use('/', superSave.getRouter());
+
+  await repository.create({ name: 'Mars' });
+  await repository.create({ name: 'Earth' });
+
+  test('ascending', async () => {
+
+    const response = await supertest(app)
+      .get('/planets')
+      .query({ sort: 'name' })
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    expect(response.body.data).toBeDefined();
+    expect(Array.isArray(response.body.data)).toBe(true);
+    expect(response.body.data).toHaveLength(2);
+    expect(response.body.data[0].name).toBe('Earth');
+  });
+
+  test('descending', async () => {
+
+    const response = await supertest(app)
+      .get('/planets')
+      .query({ sort: '-name' })
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    expect(response.body.data).toBeDefined();
+    expect(Array.isArray(response.body.data)).toBe(true);
+    expect(response.body.data).toHaveLength(2);
+    expect(response.body.data[0].name).toBe('Mars');
+  });
 });
